ef5240f4c8fa92e2c2ec9352a006fe0329707441
Dany Haddad <danyhaddad43@gmail.com>
2019-02-06 12:43:44 -0600

    support empty list args

--- rabbit_ml/arg_parsers.py
+++ rabbit_ml/arg_parsers.py
@@ -1,7 +1,7 @@
 from typing import Callable, Any
 
 def list_arg(cast_elem: Callable[[str], Any]):
-  return lambda cli_string: [cast_elem(elem) for elem in cli_string.split(',')]
+  return lambda cli_string: [cast_elem(elem) for elem in cli_string.split(',') if elem != 'EMPTY']
 
 def optional_arg(cast_elem: Callable[[str], Any]):
   return lambda cli_string: cast_elem(cli_string) if cli_string is not None else None
